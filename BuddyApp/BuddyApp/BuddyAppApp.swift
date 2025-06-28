48e65dfc33cde0f310bea2ef8491145759426873
SeminarBUW <seminarbuw25@gmail.com>
2025-06-28 11:42:53 +0200

    Add files via upload

---
+++ BuddyApp/BuddyApp/BuddyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyAppApp.swift
+//  BuddyApp
+//
+//  Created by Urtak Hoti on 28.06.25.
+//
+
+import SwiftUI
+
+@main
+struct BuddyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuddyApp/BuddyAppTests/BuddyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyAppTests.swift
+//  BuddyAppTests
+//
+//  Created by Urtak Hoti on 28.06.25.
+//
+
+import Testing
+@testable import BuddyApp
+
+struct BuddyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
